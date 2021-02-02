f8fc7fb964fbb2b23ec414a2c8a79c800c6d482a
ssbeast <ssbeast007@gmail.com>
2021-02-02 09:07:19 +0530

    User avatar uploaded base64

--- src/routers/task.js
+++ src/routers/task.js
@@ -101,24 +101,4 @@ router.delete('/tasks/:id', auth, async (req, res) => {
     }
 })
 
-const upload = multer({
-    dest: 'avatars',
-    limits: {
-        fileSize: 1000000
-    },
-    fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-            return cb(new Error('Please upload an image'))
-        }
-
-        cb(undefined, true)
-    }
-})
-
-router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
-    res.send()
-}, (error, req, res, next) => {
-    res.status(400).send({ error: error.message })
-})
-
 module.exports = router
