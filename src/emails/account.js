0d34781427d154ea1442fb89c3ca90724c5641ad
ssbeast <ssbeast007@gmail.com>
2021-02-02 11:44:01 +0530

    Dev env setup

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,13 +1,11 @@
 const sgMail = require('@sendgrid/mail')
 
-const sendgridAPIKey = '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'
-
-sgMail.setApiKey(sendgridAPIKey)
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'andrew@mead.io',
+        from: 'ssbeast007@gmail.com',
         subject: 'Thanks for joining in!',
         text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
     })
@@ -16,7 +14,7 @@ const sendWelcomeEmail = (email, name) => {
 const sendCancelationEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'andrew@mead.io',
+        from: 'ssbeast007@gmail.com',
         subject: 'Sorry to see you go!',
         text: `Goodbye, ${name}. I hope to see you back sometime soon.`
     })
