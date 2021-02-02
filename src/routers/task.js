ba227abe323cf1657ca8a64ffae2c5807c83f4d1
ssbeast <ssbeast007@gmail.com>
2021-02-02 08:38:34 +0530

    user avatar uploaded

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const multer = require('multer')
 const Task = require('../models/task')
 const auth = require('../middleware/auth')
 const router = new express.Router()
@@ -98,6 +99,14 @@ router.delete('/tasks/:id', auth, async (req, res) => {
     } catch (e) {
         res.status(500).send()
     }
+})
+
+const upload = multer({
+    dest: 'avatars'
+})
+
+router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+    res.send()
 })
 
 module.exports = router
