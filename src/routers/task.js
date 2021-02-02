b3212f8f182adbd14ea60350ff29ff0e9fd8417e
ssbeast <ssbeast007@gmail.com>
2021-02-02 08:47:08 +0530

    Minor changes

--- src/routers/task.js
+++ src/routers/task.js
@@ -102,7 +102,17 @@ router.delete('/tasks/:id', auth, async (req, res) => {
 })
 
 const upload = multer({
-    dest: 'avatars'
+    dest: 'avatars',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload an image'))
+        }
+
+        cb(undefined, true)
+    }
 })
 
 router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
